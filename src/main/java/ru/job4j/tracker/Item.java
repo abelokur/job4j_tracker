158cdb4a01809e4b0e0ec1777d0028e5b677f966
abelokur <abelokur@yandex.ru>
2020-09-06 22:29:46 +0300

    7. Date. Отображение даты. [#345200]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,13 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
+
 
     public int getId() {
         return id;
@@ -18,5 +23,13 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getNow() {return created;}
+
+    public void StartUi() {
+        Item Item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(Item.getNow().format(formatter));
     }
 }
