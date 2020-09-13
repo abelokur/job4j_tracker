36c6013c5fffb92af8d3b619744d5922dccafacc
abelokur <abelokur@yandex.ru>
2020-09-13 19:48:49 +0300

    5. Tracker - хранилище [#345236]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,8 +2,12 @@ package ru.job4j.tracker;
 
 public class StartUI {
     public static void main(String[] args) {
+        Tracker tracker = new Tracker();
         Item item = new Item();
-        System.out.println(item);
+        item.setId(1);
+        item.setName("Andrey");
+        tracker.add(item);
+        System.out.println(tracker.findById(1));
     }
 
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,27 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        int size = 0;
+        Item[] r_items = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            if (items[index].getName() != null) {
+                r_items[size++] = items[index];
+            }
+        }
+        return Arrays.copyOf(r_items, size);
+    }
+
+    public Item[] findByName(String key) {
+        int size = 0;
+        Item[] r_items = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            if (items[index].getName().equals(key)) {
+                r_items[size++] = items[index];
+            }
+        }
+        return Arrays.copyOf(r_items, size);
     }
 }
