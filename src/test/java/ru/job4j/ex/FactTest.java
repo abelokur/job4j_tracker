1463e6cb6360c349a5ee66277114e38981d72db5
abelokur <abelokur@yandex.ru>
2020-10-15 21:33:22 +0300

    0.3. Тестирование исключений с junit [#345176]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenStartGreaterThenFinish() {
+        Fact.calc(-3);
+
+    }
+}
